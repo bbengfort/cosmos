dd27da5fb61782f534623570d34dc5d1d8b0d7df
Benjamin Bengfort <benjamin@bengfort.com>
2022-03-20 14:57:11 -0500

    ci/cd

---
+++ containers/build.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+ask() {
+    local prompt default reply
+
+    if [[ ${2:-} = 'Y' ]]; then
+        prompt='Y/n'
+        default='Y'
+    elif [[ ${2:-} = 'N' ]]; then
+        prompt='y/N'
+        default='N'
+    else
+        prompt='y/n'
+        default=''
+    fi
+
+    while true; do
+
+        # Ask the question (not using "read -p" as it uses stderr not stdout)
+        echo -n "$1 [$prompt] "
+
+        # Read the answer (use /dev/tty in case stdin is redirected from somewhere else)
+        read -r reply </dev/tty
+
+        # Default?
+        if [[ -z $reply ]]; then
+            reply=$default
+        fi
+
+        # Check if the reply is valid
+        case "$reply" in
+            Y*|y*) return 0 ;;
+            N*|n*) return 1 ;;
+        esac
+
+    done
+}
+
+# Get the tag as the first argument or from git if none is supplied
+if [ -z "$1" ]; then
+    TAG=$(git rev-parse --short HEAD)
+else
+    TAG=$1
+fi
+
+# Confirm that we're continuing with the tag
+if ! ask "Continue with tag $TAG?" N; then
+    exit 1
+fi
+
+# Set some helpful variables
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+REPO=$(realpath "$DIR/..")
+DOTENV="$REPO/.env"
+
+# Load .env file from project root if it exists
+if [ -f $DOTENV ]; then
+    set -o allexport
+    source $DOTENV
+    set +o allexport
+fi
+
+# Build the primary images
+docker build -t bbengfort/celeste:$TAG -f $DIR/celeste/Dockerfile $REPO
+
+# Push to Dockerhub
+docker push bbengfort/celeste:$TAG
